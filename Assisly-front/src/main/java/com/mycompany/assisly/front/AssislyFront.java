cb892c169ddfa7d3a8c4abb9b0f333aafb20a2d7
paulinacarrero <paulinacarrero123@gmail.com>
2026-03-04 19:26:48 -0500

    Updating project

---
+++ Assisly-front/src/main/java/com/mycompany/assisly/front/AssislyFront.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.assisly.front;
+
+/**
+ *
+ * @author Paulina
+ */
+public class AssislyFront {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
